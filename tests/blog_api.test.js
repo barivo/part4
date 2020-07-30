ab500596b6cf0410cb3e9ff185feef4d81b63151
v <v@vs-MacBook-Pro.local>
2020-07-30 19:26:41 -0400

    added supertest

---
+++ tests/blog_api.test.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("there is one blog entry", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(1);
+});
+
+test("blog title to be 'First Post'", async () => {
+  const response = await api.get("/api/blogs");
+  console.log(response.body);
+  expect(response.body[0].title).toContain("First");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
